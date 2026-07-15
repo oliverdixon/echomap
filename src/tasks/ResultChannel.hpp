c469917ec007ac6d933cf32b201981221d13d3a3
Oliver Dixon <od641@york.ac.uk>
2026-07-15 16:31:35 +0100

    Add a result channel to manage emission of signals.

---
+++ src/tasks/ResultChannel.hpp
@@ -0,0 +1,113 @@
+/**
+ * @file
+ *
+ * ResultChannel specification
+ *
+ * @author Oliver Dixon
+ * @date 2026-07-15
+ */
+
+#ifndef ECHOMAP_RESULTCHANNEL_HPP
+#define ECHOMAP_RESULTCHANNEL_HPP
+
+#include <sigc++/signal.h>
+
+#include <optional>
+
+namespace echomap
+{
+
+/**
+ * Maintains a type-safe multi-observer single-consumer message bus for WorkerResult objects.
+ *
+ * @tparam ResultT The type of result to broadcast on the channel. In practice, this is the first parameter of the
+ *  subscribed slots.
+ */
+template <class ResultT> class ResultChannel
+{
+public:
+    /**
+     * Provide a new slot as an observer on the channel.
+     *
+     * @tparam SlotT The slot type signature, accepting <code>const ResultT&</code>.
+     * @param slot The slot to add to the subscribers of the channel.
+     * @return The connection between the slot and the signal.
+     */
+    template <class SlotT>
+    sigc::connection observe(
+            SlotT&& slot
+    )
+    {
+        return signal.connect(std::forward<SlotT>(slot));
+    }
+
+    /**
+     * Publish a consumable result onto the channel for all observers, followed by the single consumer.
+     *
+     * @param result An r-value reference to the result to publish on the channel.
+     */
+    void publish(
+            ResultT&& result
+    )
+    {
+        std::exception_ptr observer_exception;
+
+        // Emit const ResultT& to all observers.
+        try {
+            publish(result);
+        } catch (...) {
+            observer_exception = std::current_exception();
+        }
+
+        // If there is a nominated consumer, send it out.
+        if (consumer.has_value())
+            (*consumer)(std::move(result));
+
+        if (observer_exception)
+            std::rethrow_exception(observer_exception);
+    }
+
+    /**
+     * Publish a non-consumable result onto the channel for all observers.
+     *
+     * @param result A constant l-value reference to the result to publish on the channel.
+     */
+    void publish(
+            const ResultT& result
+    )
+    {
+        signal.emit(result);
+    }
+
+    /**
+     * Nominate the consumer slot for the channel.
+     *
+     * @tparam SlotT The slot type signature, accepting <code>ResultT&&</code>.
+     * @param slot The slot to consume the result following broadcast to observers.
+     *
+     * @throws std::runtime_error The channel already had a nominated consumer.
+     * @warning This function may only be called once for each channel; otherwise, an exception will be thrown.
+     */
+    template <class SlotT>
+        requires(!std::is_const_v<ResultT>)
+    void nominate_consumer(
+            SlotT&& slot
+    )
+    {
+        if (consumer.has_value())
+            throw std::logic_error("Result channel already has a nominated consumer.");
+
+        consumer.emplace(std::forward<SlotT>(slot));
+    }
+
+private:
+    using ObserverSignalT = sigc::signal<void(const ResultT&)>;
+    using ConsumerSignalT = sigc::slot<void(ResultT&&)>;
+
+    ObserverSignalT signal;
+    std::optional<ConsumerSignalT> consumer;
+};
+
+} // namespace echomap
+
+#endif // ECHOMAP_RESULTCHANNEL_HPP
